5e17767e494a2148768e0ea80680ae6166a32175
Mathieu Méa <mathieu.mea@gmail.com>
2015-10-21 13:16:03 -0400

    Fix trip directions...

--- src/org/mtransit/parser/ca_halifax_transit_ferry/HalifaxTransitFerryAgencyTools.java
+++ src/org/mtransit/parser/ca_halifax_transit_ferry/HalifaxTransitFerryAgencyTools.java
@@ -1,20 +1,29 @@
 package org.mtransit.parser.ca_halifax_transit_ferry;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.Pair;
+import org.mtransit.parser.SplitUtils;
 import org.mtransit.parser.Utils;
+import org.mtransit.parser.SplitUtils.RouteTripSpec;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
+import org.mtransit.parser.gtfs.data.GTripStop;
 import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRoute;
+import org.mtransit.parser.mt.data.MTripStop;
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.mt.data.MTrip;
 
@@ -85,7 +94,7 @@ public class HalifaxTransitFerryAgencyTools extends DefaultAgencyTools {
 		} else if (FER_W.equals(gRoute.getRouteShortName())) {
 			return RID_WS;
 		}
-		System.out.println("Unexpected route ID " + gRoute);
+		System.out.printf("\nUnexpected route ID  for %s %s!\n", gRoute);
 		System.exit(-1);
 		return -1l;
 	}
@@ -100,7 +109,7 @@ public class HalifaxTransitFerryAgencyTools extends DefaultAgencyTools {
 		} else if (FER_W.equals(gRoute.getRouteShortName())) {
 			return RLN_WS;
 		}
-		System.out.println("Unexpected route long name " + gRoute);
+		System.out.printf("\nUnexpected route long name for %s!\n", gRoute);
 		System.exit(-1);
 		return null;
 	}
@@ -115,7 +124,7 @@ public class HalifaxTransitFerryAgencyTools extends DefaultAgencyTools {
 		} else if (FER_W.equals(gRoute.getRouteShortName())) {
 			return RTS_WS;
 		}
-		System.out.println("Unexpected route short name " + gRoute);
+		System.out.printf("\nUnexpected route short name for %s!\n", gRoute);
 		System.exit(-1);
 		return null;
 	}
@@ -132,33 +141,68 @@ public class HalifaxTransitFerryAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public String getRouteColor(GRoute gRoute) {
-		return DEFAULT_ROUTE_COLOR;
+		if (StringUtils.isEmpty(gRoute.getRouteColor())) {
+			return DEFAULT_ROUTE_COLOR;
+		}
+		return super.getRouteColor(gRoute);
 	}
 
 	private static final String WOODSIDE = "Woodside";
 	private static final String DARTMOUTH = "Dartmouth";
 	private static final String HALIFAX = "Halifax";
 
+	private static HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
+	static {
+		HashMap<Long, RouteTripSpec> map2 = new HashMap<Long, RouteTripSpec>();
+		map2.put(RID_ALD, new RouteTripSpec(RID_ALD, //
+				0, MTrip.HEADSIGN_TYPE_STRING, DARTMOUTH, //
+				1, MTrip.HEADSIGN_TYPE_STRING, HALIFAX) //
+				.addTripSort(0, //
+						Arrays.asList(new String[] { "1073", "1074" })) //
+				.addTripSort(1, //
+						Arrays.asList(new String[] { "1074", "1073" })) //
+				.compileBothTripSort());
+		map2.put(RID_WS, new RouteTripSpec(RID_WS, //
+				0, MTrip.HEADSIGN_TYPE_STRING, HALIFAX, //
+				1, MTrip.HEADSIGN_TYPE_STRING, WOODSIDE) //
+				.addTripSort(0, //
+						Arrays.asList(new String[] { "1075", "1073" })) //
+				.addTripSort(1, //
+						Arrays.asList(new String[] { "1073", "1075" })) //
+				.compileBothTripSort());
+		ALL_ROUTE_TRIPS2 = map2;
+	}
+
+	@Override
+	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
+		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
+			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+		}
+		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+	}
+
+	@Override
+	public ArrayList<MTrip> splitTrip(MRoute mRoute, GTrip gTrip, GSpec gtfs) {
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return ALL_ROUTE_TRIPS2.get(mRoute.getId()).getAllTrips();
+		}
+		return super.splitTrip(mRoute, gTrip, gtfs);
+	}
+
+	@Override
+	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()));
+		}
+		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
+	}
+
 	@Override
 	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
-		if (mRoute.getId() == RID_ALD) {
-			if (gTrip.getDirectionId() == 0) {
-				mTrip.setHeadsignString(DARTMOUTH, gTrip.getDirectionId());
-				return;
-			} else if (gTrip.getDirectionId() == 1) {
-				mTrip.setHeadsignString(HALIFAX, gTrip.getDirectionId());
-				return;
-			}
-		} else if (mRoute.getId() == RID_WS) {
-			if (gTrip.getDirectionId() == 0) {
-				mTrip.setHeadsignString(HALIFAX, gTrip.getDirectionId());
-				return;
-			} else if (gTrip.getDirectionId() == 1) {
-				mTrip.setHeadsignString(WOODSIDE, gTrip.getDirectionId());
-				return;
-			}
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return; // split
 		}
-		System.out.println("Unexpected trip " + gTrip);
+		System.out.printf("\nUnexpected trip %s!\n", gTrip);
 		System.exit(-1);
 	}
 
